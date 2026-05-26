75d5d88c922c1e54d7cff4015f82b32751e0b24d
James N. V. Cash <james.cash@occasionallycogent.com>
2026-05-26 11:14:39 -0400

    Let there be a module

---
+++ teamtype.el
@@ -0,0 +1,30 @@
+;;; teamtype.el --- Emacs module for TeamType collaborative editing  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  Jamie Cash
+
+;; Author: Jamie Cash <jamie@occasionallycogent.com>, blinry <mail@blinry.org>
+;; Keywords: tools, data, comm
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Emacs integration for Teamtype (https://teamtype.github.io/teamtype/introduction.html)
+
+;;; Code:
+
+
+
+(provide 'teamtype)
+;;; teamtype.el ends here
